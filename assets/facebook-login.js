c0b71d08e2bb70d051a3e21c1b9b314b4b96cc96
eyal <eyal@dudamobile.com>
2015-04-04 13:44:52 +0300

    changed login html added fb login sinpets (facebook)

---
+++ assets/facebook-login.js
@@ -0,0 +1,52 @@
+/*  FACEBOOK ------------------------------------- */
+
+function statusChangeCallback(response) {
+  console.log('------- statusChangeCallback -------');
+  console.log('response' + response);
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '906060826121111',
+      cookie     : true,  // enable cookies to allow the server to access 
+                          // the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.2' // use version 2.2
+    });
+
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/he_IL/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+    });
+}
+
+
+/*  SHOPIFY ------------------------------------- */
